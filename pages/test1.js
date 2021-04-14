5765ea01069a32b90c5de0ba575ceec2de5af091
sjagoori <sjagoori@users.noreply.github.com>
2021-04-14 23:08:35 +0200

    + player

---
+++ pages/test1.js
@@ -0,0 +1,109 @@
+import Head from 'next/head'
+import { useRef } from 'react'
+import styled from 'styled-components'
+import { useSpring, animated } from 'react-spring'
+import { useDrag } from 'react-use-gesture'
+import Block from 'components/block/Block.js'
+import ReactAudioPlayer from 'react-audio-player';
+import Toast from 'light-toast';
+
+
+export default function Home() {
+  let rap = useRef(null);
+  let direction = useRef(null);
+  let isPlaying;
+
+  // Set the drag hook and define component movement based on gesture data
+  const [{ x, y, scale }, api] = useSpring(() => ({ x: 0, y: 0, scale: 1 }))
+  const bind = useDrag(
+    async ({ dragging, active, movement: [mx, my], tap, cancel }) => {
+      if (mx > 50 || mx < -50) cancel()
+      if (my > 50 || my < -50) cancel()
+      api.start({ x: active ? mx : 0, y: active ? my : 0, immediate: active, scale: active ? 1.2 : 1})
+
+      if (my < -10 && (rap.current.audioEl.current.volume <= 0.8 && rap.current.audioEl.current.volume >= 0 && !dragging)){
+        rap.current.audioEl.current.volume = rap.current.audioEl.current.volume + 0.2
+        rap.current.audioEl.current.volume == 1 ? Toast.fail('Max volume', 500) : Toast.info('Volume up', 500)
+        Toast.hide()
+      }
+
+      if (my > 10 && (rap.current.audioEl.current.volume <= 1 && rap.current.audioEl.current.volume > 0.2) && !dragging){
+        rap.current.audioEl.current.volume = rap.current.audioEl.current.volume - 0.2 
+        rap.current.audioEl.current.volume == 5.5 ? Toast.fail('Min volume', 500) : Toast.info('Volume down', 500)
+
+        Toast.info('Volume down', 500)
+        Toast.hide()
+      }
+
+      if (mx > 10 && !dragging){
+        Toast.info('Skip song', 500)
+      }
+
+      if (mx < -10 && !dragging){
+        Toast.info('Previous song', 500)
+      }
+
+      console.log(rap.current.audioEl.current.volume)
+
+      if (tap && !isPlaying) {
+        await rap.current.audioEl.current.play()
+        isPlaying = true
+        Toast.info('Playing', 500)
+        Toast.hide()
+      } else if (tap && isPlaying) {
+        rap.current.audioEl.current.pause()
+        isPlaying = false
+        Toast.info('Paused', 500)
+        Toast.hide()
+      }
+    },
+    { lockDirection: true, filterTaps: true, threshold: 1 })
+
+
+    // console.log('direction', Toast.info(direction.current, 1000))
+
+  return (
+    <Centered>
+      <ReactAudioPlayer
+        src="George_Gershwin_playing_Rhapsody_in_Blue.ogg"
+        // controls
+        ref={rap}
+        id='player'
+      // onPlay={(e) => console.log(e)}
+      />
+      <animated.div {...bind(Block)} 
+      style={{ x, y, scale, touchAction: 'none', width: '100px', height: '100px', backgroundColor: '#434190', borderRadius: '10px' }} 
+      onDragEnd={() => {
+        console.log('drop')
+        console.log(direction)
+      }}
+      />
+    </Centered>
+  )
+}
+
+const Centered = styled.div` 
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`
+
+
+
+// const bind = useDrag(({ down, movement: [mx, my] }) => {
+//   set({ x: down ? mx : 0, y: down ? my : 0 },{ lockDirection: true })
+//   console.log(down)
+//   // console.log(mx, my)
+
+//   // if (my < -80) console.log('going up')
+//   // if (mx < -80 && my < -80) console.log('going up-left')
+//   // if (mx < -80 && my > 80) console.log('going up-right')
+
+//   // if (my > 80) console.log('going down')
+//   // if (my > 80 && mx < -80) console.log('going down-left')
+//   // if (my > 80 && mx > 80) console.log('going down-right')
+
+//   // if (mx > 80) console.log('going right')
+//   // if (mx < -80) console.log('going left')
+// })
