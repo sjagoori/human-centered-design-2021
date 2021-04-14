84dec1c3055ecf88db48e23e60707339d90cf0db
sjagoori <sjagoori@users.noreply.github.com>
2021-04-14 23:08:13 +0200

    + menu to prototypes

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,24 @@
-import Head from 'next/head'
-import { useRef } from 'react'
+import Link from 'next/link'
 import styled from 'styled-components'
-import { useSpring, animated } from 'react-spring'
-import { useDrag } from 'react-use-gesture'
-import Block from 'components/block/Block.js'
-import ReactAudioPlayer from 'react-audio-player';
 
 
 export default function Home() {
-  let rap = useRef(null);
-  let isPlaying;
-
-  // Set the drag hook and define component movement based on gesture data
-  const [{ x, y, scale }, api] = useSpring(() => ({ x: 0, y: 0, scale: 1 }))
-  const bind = useDrag(
-    async ({ down, movement: [mx, my], tap }) => {
-      api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down, scale: down ? 1.2 : 1 })
-      if (my < 80) console.log('up')
-      if (my > 80) console.log('down')
-      if (my < 80 && (rap.current.audioEl.current.volume <= 0.8 && rap.current.audioEl.current.volume >= 0)) rap.current.audioEl.current.volume = rap.current.audioEl.current.volume + 0.2
-      if (my > 80 && (rap.current.audioEl.current.volume <= 1 && rap.current.audioEl.current.volume > 0.2)) rap.current.audioEl.current.volume = rap.current.audioEl.current.volume - 0.2
-
-      console.log(rap.current.audioEl.current.volume)
-
-      if (tap && !isPlaying) {
-        await rap.current.audioEl.current.play()
-        isPlaying = true
-        console.log(tap)
-
-      } else if (tap && isPlaying) {
-        rap.current.audioEl.current.pause()
-        isPlaying = false
-      }
-
-      // if (!rap.current.audioEl.current.play) 
-
-      // console.dir(await rap.current.audioEl.current.play())
-
-      // working
-      // rap.current.audioEl.current.volume = 0.2
-    },
-    { lockDirection: true, filterTaps: true })
-
-
 
   return (
     <Centered>
-      <ReactAudioPlayer
-        src="George_Gershwin_playing_Rhapsody_in_Blue.ogg"
-        controls
-        ref={rap}
-        id='player'
-      // onPlay={(e) => console.log(e)}
-      />
-      <animated.div {...bind(Block)} style={{ x, y, scale, touchAction: 'none', width: '100px', height: '100px', backgroundColor: 'black', borderRadius: '5px' }} />
+      <Link href='/test1'>
+        Prototype 1
+      </Link>
+      <Link href='/test2'>
+        Prototype 2
+      </Link>
     </Centered>
   )
 }
 
 const Centered = styled.div`
   display: flex;
+  flex-direction: column;
   height: 100vh;
   align-items: center;
   justify-content: center;
