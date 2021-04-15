132f9f1d50adc9c58041defa5e11ce124ed69bfc
sjagoori <sjagoori@users.noreply.github.com>
2021-04-15 13:19:20 +0200

    + toast % status

--- pages/test2.js
+++ pages/test2.js
@@ -10,11 +10,13 @@ export default function Test2() {
   let songs = ["bensound-buddy.mp3", "bensound-dubstep.mp3", "bensound-happyrock.mp3"]
   const [state, setState] = useState(false)
   const [song, setSong] = useState(songs[1])
-  let toastDuration = 350
+  let toastDuration = 500
 
   useHotkeys('p', async () => {
     if (!state) {
+      Toast.success('Paused', toastDuration)
       setState(true);
+      Toast.hide()
       return await rap.current.audioEl.current.play();
     } else if (state) {
       setState(false);
@@ -24,7 +26,9 @@ export default function Test2() {
   });
 
   useHotkeys('space', () => {
+    Toast.success('Paused', toastDuration)
     setState(false);
+    Toast.hide()
     return rap.current.audioEl.current.pause();
   })
 
@@ -41,10 +45,10 @@ export default function Test2() {
   })
 
   useHotkeys('i', () => {
-    if (rap.current.audioEl.current.volume <= 1 && rap.current.audioEl.current.volume > 0.2) {
+    if (rap.current.audioEl.current.volume <= 1 && rap.current.audioEl.current.volume >= 0.2) {
       console.log(rap.current.audioEl.current.volume)
 
-      rap.current.audioEl.current.volume <= 0.3 ? Toast.fail('Min volume', toastDuration) : Toast.info('Volume down', toastDuration)
+      rap.current.audioEl.current.volume <= 0.1 ? Toast.fail(`Min volume (${(rap.current.audioEl.current.volume.toFixed(2)*100)+'%'})`, toastDuration) : Toast.info(`Volume down (${(rap.current.audioEl.current.volume.toFixed(2)*100)+'%'})`, toastDuration)
       rap.current.audioEl.current.volume = rap.current.audioEl.current.volume - 0.2
       Toast.hide()
     }
@@ -53,7 +57,7 @@ export default function Test2() {
   useHotkeys('o', () => {
     if (rap.current.audioEl.current.volume <= 1 && rap.current.audioEl.current.volume >= 0) {
       console.log(rap.current.audioEl.current.volume)
-      rap.current.audioEl.current.volume > 0.7 ? Toast.fail('Max volume', toastDuration) : Toast.info('Volume up', toastDuration)
+      rap.current.audioEl.current.volume > 0.9 ? Toast.fail(`Max volume (${(Math.round(rap.current.audioEl.current.volume)*100)+'%'})`, toastDuration) : Toast.info(`Volume (${(Math.round(rap.current.audioEl.current.volume)*100)+'%'})`, toastDuration)
       if (rap.current.audioEl.current.volume != 1) rap.current.audioEl.current.volume = rap.current.audioEl.current.volume + 0.2
       Toast.hide()
     }
@@ -63,13 +67,22 @@ export default function Test2() {
 
   return (
     <Centered>
-      <p>{song}</p>
+      <p>▶ {song}</p>
       <ReactAudioPlayer
         src={songs[1]}
-        controls
+
         ref={rap}
         id='player'
       />
+      <div>
+        <p><code>p</code> - play</p>
+        <p><code>space</code> - pause</p>
+        <p><code>]</code> - next song</p>
+        <p><code>[</code> - previous song</p>
+        <p><code>i</code> - volume down</p>
+        <p><code>o</code> - volume up</p>
+
+      </div>
     </Centered>
   )
 }
@@ -80,4 +93,12 @@ const Centered = styled.div`
   height: 100vh;
   align-items: center;
   justify-content: center;
+
+  code {
+    background-color: #cccccc;
+    padding: 5px 10px;
+    margin-right: 5px;
+    border-radius: 5px;
+    box-shadow: 0px 5px 3px black;
+    }
 `
