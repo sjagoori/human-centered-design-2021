3b8644b60989408fb8d097f2696a0295ad8c0c13
sjagoori <sjagoori@users.noreply.github.com>
2021-04-21 20:13:48 +0200

    + added prototype 4

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,15 @@
-import Link from 'next/link'
-import styled from 'styled-components'
-
+import Link from "next/link";
+import styled from "styled-components";
 
 export default function Home() {
-
   return (
     <Centered>
-      <Link href='/test1'>
-        Prototype 1
-      </Link>
-      <Link href='/test2'>
-        Prototype 2
-      </Link>
-      <Link href='/test3'>
-        Prototype 3
-      </Link>
+      <Link href="/test1">Prototype 1</Link>
+      <Link href="/test2">Prototype 2</Link>
+      <Link href="/test3">Prototype 3</Link>
+      <Link href="/test4">Prototype 4</Link>
     </Centered>
-  )
+  );
 }
 
 const Centered = styled.div`
@@ -33,15 +26,13 @@ const Centered = styled.div`
     border-radius: 5px;
     margin-bottom: 10px;
 
-    &:nth-child(-n+2){
+    &:nth-child(-n + 2) {
       background-color: transparent;
       border: 1px solid #c1c1c1;
       color: #c1c1c1;
     }
   }
-`
-
-
+`;
 
 // const bind = useDrag(({ down, movement: [mx, my] }) => {
 //   set({ x: down ? mx : 0, y: down ? my : 0 },{ lockDirection: true })
