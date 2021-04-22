d8cfcf4df16fa582e76080d61ecf95fed65a18cc
sjagoori <sjagoori@users.noreply.github.com>
2021-04-22 15:45:52 +0200

    ~ icon sizes

--- assets/svg/PlayPause.js
+++ assets/svg/PlayPause.js
@@ -2,10 +2,10 @@ export default function PlayPause() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      enable-background="new 0 0 30 30"
-      height="30px"
-      viewBox="0 0 30 30"
-      width="30px"
+      enable-background="new 0 0 24 24"
+      height="36px"
+      viewBox="0 0 24 24"
+      width="36px"
       fill="#000000"
     >
       <path fill="currentColor" d="M3,5V19L11,12M13,19H16V5H13M18,5V19H21V5" />
--- assets/svg/VolumeDown.js
+++ assets/svg/VolumeDown.js
@@ -2,9 +2,9 @@ export default function VolumeDown() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      height="24px"
+      height="36px"
       viewBox="0 0 24 24"
-      width="24px"
+      width="36px"
       fill="#000000"
     >
       <path d="M0 0h24v24H0V0z" fill="none" />
